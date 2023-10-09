ca3fb9e52144d1040c8556ebf9b1d8d40a1e13fa
Ken Takahashi <takaken@redimpulz.com>
2023-10-09 13:20:59 +0900

    promiseのテストを追加

--- promise/script.js
+++ promise/script.js
@@ -1,38 +1,28 @@
 // 非同期処理
-const func1 = async () => {
-  console.log("start");
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+export const func1 = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
   const json = await res.json();
-  console.log(json);
-  console.log("finish");
+  return json;
 };
 
 // 直列で非同期処理
-const func2 = async () => {
+export const func2 = async () => {
   const ids = [1, 2, 3, 4, 5];
   for (const id of ids) {
-    console.log(`start ${id}`);
     const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
     const json = await res.json();
     console.log(json);
-    console.log(`finish ${id}`);
   }
 };
 
 // 並列で非同期処理
-const func3 = async () => {
+export const func3 = async () => {
   const ids = [1, 2, 3, 4, 5];
   const fetches = ids.map((id) =>
     fetch(`https://jsonplaceholder.typicode.com/todos/${id}`).then((res) =>
       res.json()
     )
   );
-  console.log("start");
   const jsons = await Promise.all(fetches);
-  console.log(jsons);
-  console.log("finish");
+  return jsons;
 };
-
-func1();
-func2();
-func3();
