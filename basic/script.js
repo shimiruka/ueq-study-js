ce53c1cf5f67b5c9717961dbd56104c4f6e3ae04
Ken Takahashi <takaken@redimpulz.com>
2023-11-07 14:39:17 +0900

    回答を修正

--- basic/script.js
+++ basic/script.js
@@ -1,5 +1,7 @@
-// ループ・配列
+// ループ・配列・データ型の問題
 export const func1 = (n) => {
+  if (!Number.isInteger(n)) return [];
+  if (n <= 0) return [];
   const arr = [];
   for (let i = 0; i < n; i++) {
     arr.push(i + 1);
@@ -7,9 +9,10 @@ export const func1 = (n) => {
   return arr;
 };
 
-// 条件分岐・データ型
+// 条件分岐・データ型の問題
 export const func2 = (n) => {
-  if (typeof n !== "number") return "error";
+  if (!Number.isInteger(n)) return "error";
+  if (n <= 0) return "error";
   if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
   if (n % 3 === 0) return "Fizz";
   if (n % 5 === 0) return "Buzz";
