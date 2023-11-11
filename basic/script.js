19d46182edcc716f858b67ff0accbc5322654b55
shimiruka <iruka374@gmail.com>
2023-11-11 11:16:45 +0000

    課題のコードを作成

--- basic/script.js
+++ basic/script.js
@@ -1,9 +1,23 @@
 // ループ・配列・データ型の問題
 export const func1 = (n) => {
-  // このコードを完成させてください
+  if (!Number.isInteger(n) || n <= 0) {
+    return [];
+  }
+  return Array.from({ length: n }, (_, i) => i + 1);
 };
 
 // 条件分岐・データ型の問題
 export const func2 = (n) => {
-  // このコードを完成させてください
+  if (!Number.isInteger(n) || n <= 0) {
+    return 'error';
+  }
+  if (n % 3 == 0 && n % 5 == 0) {
+    return 'FizzBuzz';
+  } else if (n % 3 == 0) {
+    return 'Fizz';
+  } else if (n % 5 == 0) {
+    return 'Buzz';
+  } else {
+    return n.toString();
+  }
 };
