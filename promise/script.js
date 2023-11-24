c415ff944fcc4b45962afeb24bb818d120776ef9
shimiruka <iruka374@gmail.com>
2023-11-24 08:51:30 +0000

    課題のコードを作成

--- promise/script.js
+++ promise/script.js
@@ -1,14 +1,26 @@
 // 非同期処理
 export const func1 = async () => {
-  // このコードを完成させてください
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const json = await response.json();
+  return json;
 };
 
 // 直列で非同期処理
 export const func2 = async () => {
-  // このコードを完成させてください
+  const todoIds = ["1", "2", "3", "4", "5"];
+  for (let id of todoIds) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${encodeURIComponent(id)}`);
+    const json = await response.json();
+    console.log(json);
+  }
 };
 
 // 並列で非同期処理
 export const func3 = async () => {
-  // このコードを完成させてください
+  const todoIds = ["1", "2", "3", "4", "5"];
+  const responses = await Promise.all(
+    todoIds.map((id) => fetch(`https://jsonplaceholder.typicode.com/todos/${encodeURIComponent(id)}`))
+  );
+  const jsons = await Promise.all(responses.map((response) => response.json()));
+  return jsons;
 };
