74e527ab31c627c38716a54b9d180f8a990ac87f
shimiruka <iruka374@gmail.com>
2023-11-24 18:15:54 +0900

    課題のコードを作成

--- dom/script.js
+++ dom/script.js
@@ -1,5 +1,16 @@
 export const main = () => {
-  // このコードを完成させてください
+  const link = document.querySelector("a");
+  link.textContent = "REDIMPULZ";
+  link.href = "https://redimpulz.com/";
+
+  const sect = document.querySelector("section");
+  const para = document.createElement("p");
+  para.textContent = "We hope you enjoy JavaScript.";
+  para.classList.add("highlight");
+  sect.appendChild(para);
+
+  const temp = sect.querySelector("#temp");
+  sect.removeChild(temp);
 };
 
 window.addEventListener("load", main);
